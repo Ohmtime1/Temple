41431972b0610f5b22d93ac96924d1030cd52976
Ohmtime <88527309+Ohmtime1@users.noreply.github.com>
2021-10-11 11:24:02 +0700

    Create Legend-For-Sea-Template.lua

---
+++ Legend-For-Sea-Template.lua
@@ -0,0 +1,39 @@
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ohmtime1/EMOkak/main/EmoNahee"))()
+local venyx = library.new("Sightless Hub V.3.7.2", 5013109572)
+
+local themes = {
+	Background = Color3.fromRGB(24, 24, 24),
+	Glow = Color3.fromRGB(0, 0, 0),
+	Accent = Color3.fromRGB(10, 10, 10),
+	LightContrast = Color3.fromRGB(20, 20, 20),
+	DarkContrast = Color3.fromRGB(14, 14, 14),  
+	TextColor = Color3.fromRGB(255, 255, 255)
+}
+
+local page = venyx:addPage("Auto Farm", 5012544693)
+
+local page2 = venyx:addPage("Auto Stat", 5012544693)
+
+local page3 = venyx:addPage("Teleport", 5012544693)
+
+local page4 = venyx:addPage("Players", 5012544693)
+
+local page5 = venyx:addPage("Dungeons", 5012544693)
+
+local page6 = venyx:addPage("Misc", 5012544693)
+
+local page7 = venyx:addPage("Credit", 5012544693)
+
+local section1 = page:addSection("Coming Soon..")
+
+local section2 = page2:addSection("Coming Soon..")
+
+local section3 = page3:addSection("Coming Soon..")
+
+local section4 = page4:addSection("Coming Soon..")
+
+local section5 = page5:addSection("Coming Soon..")
+
+local section6 = page6:addSection("Coming Soon..")
+
+local section7 = page7:addSection("Coming Soon..")
